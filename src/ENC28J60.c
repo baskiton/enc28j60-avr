8e2834c9a6339eff5918b0caee76374dc1c25274
baskiton <baskiton@mail.ru>
2020-10-15 01:35:08 +0700

    Bug Fix:     enc28j60_set_rx_mode used logical "AND" instead     of bitwise (facepalm) to determine rx mode...

--- src/ENC28J60.c
+++ src/ENC28J60.c
@@ -462,13 +462,13 @@ void enc28j60_set_rx_mode(struct net_dev_s *net_dev) {
     struct enc28j60_dev *priv = net_dev->priv;
     uint8_t mode = 0;
 
-    if (net_dev->flags.rx_mode && RX_RT_PROMISC) {
+    if (net_dev->flags.rx_mode & RX_RT_PROMISC) {
         mode = 0x00;
-    } else if (net_dev->flags.rx_mode && RX_RT_ALLMULTI) {
+    } else if (net_dev->flags.rx_mode & RX_RT_ALLMULTI) {
         mode = _BV(UCEN) | _BV(CRCEN) | _BV(MCEN) | _BV(BCEN);
-    // } else if (net_dev->flags.rx_mode && RX_RT_UNICAST) {
+    // } else if (net_dev->flags.rx_mode & RX_RT_UNICAST) {
     //     mode = _BV(UCEN) | _BV(CRCEN);
-    // } else if (net_dev->flags.rx_mode && RX_RT_MULTICAST) {
+    // } else if (net_dev->flags.rx_mode & RX_RT_MULTICAST) {
     //     mode = _BV(UCEN) | _BV(CRCEN) | _BV(MCEN);
     } else {    // RX_RT_BROADCAST - 0 by default
         mode = _BV(UCEN) | _BV(CRCEN) | _BV(BCEN);
@@ -506,6 +506,7 @@ static void enc28j60_packet_receive(struct enc28j60_dev *priv) {
             printf_P(PSTR("Out of memory for RX buffer. Need %d byte(s). Avail %ld byte(s)\n"),
                      rsv.rx_byte_cnt, get_free_mem_size());
         } else {
+            printf_P(PSTR("was allocated %u bytes\n"), rsv.rx_byte_cnt);
             rbm(priv, put_net_buff(data, rsv.rx_byte_cnt), rsv.rx_byte_cnt,
                 rand_acc_addr_calc(priv->packet_ptr, ENC28J60_RSV_SIZE));
             data->protocol = eth_type_proto(data, net_dev);
@@ -517,6 +518,7 @@ static void enc28j60_packet_receive(struct enc28j60_dev *priv) {
              * ATTENTION:
              *  IN THE HANDLER, YOU MUST REMEMBER TO FREE THE ALLOCATED MEMORY.
              */
+            free_net_buff(data);
         }
     }
 
