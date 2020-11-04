40b548a5e9c679b0946e556286a1496fd3078586
baskiton <baskiton@mail.ru>
2020-11-05 03:52:57 +0700

    Moved enc28j60_net_dev_ops structure to PROGMEM.

--- src/ENC28J60.c
+++ src/ENC28J60.c
@@ -881,8 +881,7 @@ static int8_t enc28j60_open(struct net_dev_s *net_dev) {
     return 0;
 }
 
-/** TODO: move this to PROGMEM */
-static const struct net_dev_ops_s enc28j60_net_dev_ops = {
+static const struct net_dev_ops_s enc28j60_net_dev_ops PROGMEM = {
     .init = NULL,
     .open = enc28j60_open,
     .stop = enc28j60_disable,
